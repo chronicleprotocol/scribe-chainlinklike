e70b497e5f1bff70d1ccf7419f9dba427433b42d
pascal <pascal@merkleplant.xyz>
2024-10-22 13:44:47 +0200

    src: Update deployment name

--- script/ScribeChainlinkLike.s.sol
+++ script/ScribeChainlinkLike.s.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.16;
 import {Script} from "forge-std/Script.sol";
 import {console2 as console} from "forge-std/console2.sol";
 
-import {ScribeChainlinkLike_COUNTER as ScribeChainlinkLike} from
+import {ChronicleScribeAdapter_BASE_QUOTE_COUNTER as ScribeChainlinkLike} from
     "src/ScribeChainlinkLike.sol";
-// @todo                    ^^^^^^^ Adjust counter of ScribeChainlinkLike instance.
+// @todo                       ^^^^ ^^^^^ ^^^^^^^ Adjust name of ScribeChainlinkLike instance.
 
 /**
  * @notice ScribeChainlinkLike Management Script
